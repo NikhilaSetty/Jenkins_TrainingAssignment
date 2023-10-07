ba8820a39482e43cf194c7daaf883079b9add147
Naralasetty Nikhila <naralasetty.nikhila@nagarro.com>
2023-10-07 13:25:38 +0530

    Jenkins Assignment Sample Code

--- src/main/java/sample/TestCode.java
+++ src/main/java/sample/TestCode.java
@@ -1,5 +1,7 @@
 package sample;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
@@ -7,13 +9,13 @@ import org.testng.annotations.Test;
 
 public class TestCode {
 	@Test
-	public void lauchBrowser() throws Exception
+	public void lauchBrowser() throws Exception 
 	{
 		System.setProperty("webdriver.edge.driver","C:\\Users\\naralasettynikhila\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
-		Assert.assertTrue(driver.getTitle().contains("Google"),"Title match");
+		AssertJUnit.assertTrue(driver.getTitle().contains("Google"));
 		driver.get("https://www.jenkins.io");
 		Thread.sleep(3000);
 		driver.quit();
